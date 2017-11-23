e3d17ca67491b298a1d7a46ed4a02d48af596491
Junior Castro <devcastrojunin@gmail.com>
2017-11-23 17:39:05 -0200

    Ajuste no hover

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	$('.slides .container').cycle({
 	    speed: 500,
 	    fx: "fade",
-	    timeout: 7000,
+	    timeout: 10000,
 	    slides: "> section",
 	    pager:   '#nav',
 	    next: "#next",
