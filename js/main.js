01406c725e76ef3ffabb8ac4e55e05377f0fea2a
Junior Castro <devcastrojunin@gmail.com>
2017-11-27 16:18:51 -0200

    Menu de navegação

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 jQuery(document).ready(function($) {
-	$('.slides .container').cycle({
-	    speed: 500,
-	    fx: "fade",
-	    timeout: 10000,
-	    slides: "> section",
-	    pager:   '#nav',
-	    next: "#next",
-	    prev: "#prev"
+	
+	// Menu click animation
+	$('#openMenu').on('click', function(event) {
+		event.preventDefault();
+		if ($('.list-navigation').is(':hidden')) {
+			$('#layer').fadeIn('slow');
+			$('.list-navigation').fadeIn('slow');
+		}else{
+			$('#layer').fadeOut('slow');
+			$('.list-navigation').fadeOut('slow');
+		}
+	});
+	$('.list-navigation li a').on('click', function() {
+		$('#layer').fadeOut('slow');
+		$('.list-navigation').fadeOut('slow');
 	});
 
+	// Button select item
 	$('.select-item').on('click', function(event) {
 		event.preventDefault();
 		$(this).toggleClass('active');
 	});
+
+	// Scroll navigation
+	$('a[href^="#"]').on('click',function (e) {
+        e.preventDefault ();        
+        var target = this.hash,                    
+            $target = $(target); 
+        
+        $('html, body').stop().animate ({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing');
+    });
+
 });
