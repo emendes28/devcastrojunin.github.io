f21196a3f15ebf6ebbc51ad918c79f14289bfc7d
Junior <junior@dotcase.com.br>
2017-11-29 02:04:18 -0200

    Modal + ajustes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 jQuery(document).ready(function($) {
 	
-	// Menu click animation
+	// Show and hide menu bar in scroll
+	var distance = $('section#sites').offset().top,
+    	$window = $(window);
+
+	$window.scroll(function() {
+	    if ( $window.scrollTop() >= distance ) {
+	        $('div.menu').removeClass('menu-hidden');
+	    }else{
+	    	$('div.menu').addClass('menu-hidden');
+	    }
+	});
+
+	// Show and hide menu list
 	$('#openMenu').on('click', function(event) {
 		event.preventDefault();
 		if ($('.list-navigation').is(':hidden')) {
@@ -16,21 +28,42 @@ jQuery(document).ready(function($) {
 		$('.list-navigation').fadeOut('slow');
 	});
 
+	// Scroll navigation click menu
+	$('a[href^="#"]').on('click',function (e) {
+        e.preventDefault ();        
+        var target = this.hash,                    
+            $target = $(target); 
+
+		if (target != '') {
+	        $('html, body').stop().animate ({
+	            'scrollTop': $target.offset().top
+	        }, 1000, 'swing');			
+		}                   
+    });
+
 	// Button select item
 	$('.select-item').on('click', function(event) {
 		event.preventDefault();
 		$(this).toggleClass('active');
 	});
 
-	// Scroll navigation
-	$('a[href^="#"]').on('click',function (e) {
+	// Delete item select - Modal
+	$('i.delete-item').on('click',function (e) {
         e.preventDefault ();        
-        var target = this.hash,                    
-            $target = $(target); 
-        
-        $('html, body').stop().animate ({
-            'scrollTop': $target.offset().top
-        }, 1000, 'swing');
+        var res = confirm('Deseja realmente excluir esse item?');
+
+        if (res == true) {
+        	$(this).closest('li').remove();
+			count_items();
+        }
     });
+
+	// Count items in list modal
+	function count_items(){
+		var items = $('#itemsSelected li').length;
+		$('.count-selected span').text(items);
+	}
+	count_items();
+
 
 });
